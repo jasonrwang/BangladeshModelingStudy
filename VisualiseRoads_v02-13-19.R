d76dccce6151251cd706324ed0b697dd9eb916e4
Hsin Cheng <44002425+imchengh@users.noreply.github.com>
2019-02-13 18:31:02 +0100

    Update VisualiseRoads_v02-13-19.R

--- VisualiseRoads_v02-13-19.R
+++ VisualiseRoads_v02-13-19.R
@@ -1,33 +1,42 @@
 # Data Cleaning for Road Data
 
-# Set working directory
-setwd("D:/EPA/EPA Semester-1.3/EPA1351 Advanced Discrete Simulation/Assignment 1/_Practice")
-
 # Load library
 library(dplyr)
 library(ggplot2)
 library(plotly)
 
 # Load data
-df <- read.csv("Roads_InfoAboutEachLRP.csv")
+df <- read.csv("data/Roads_InfoAboutEachLRP.csv")
 
-# Create a full list of roads
-ListOfRoad <- unique(df$road)
-ListOfRoad <- data.frame(road = ListOfRoad, RoadType = substring(ListOfRoad, 1, 1))
+## Create a full list of roads
+#ListOfRoad <- unique(df$road)
+#ListOfRoad <- data.frame(road = ListOfRoad, RoadType = substring(ListOfRoad, 1, 1))
 
 # Split raw dataframe into lists by road
 df_split <- split(df, df$road)
 
 # Modify here the road name! 
-list_SplitedRoad <- select(df_split$N102, lon, lat)
+list_SplitedRoad <- select(df_split$N6, lon, lat)
 
 # Plot the road with paths linked between points
 p <- ggplot(list_SplitedRoad, aes(lon, lat)) +
   geom_point(shape = 1) + 
   geom_path() + 
-  ggtitle("list_SplitedRoad")
+  ggtitle("Road")
 
-p <- ggplotly(p)
+ggplotly(p)
 
-p
-       
+# Calculate the Cooks distance
+cookd <- cooks.distance(aov(list_SplitedRoad$lat ~ list_SplitedRoad$lon))
+cookd <- cookd > 0.05
+plot(cookd)
+
+# save the result to the dataframe
+list_SplitedRoad$outlier <- cookd
+
+# Plot the outliers on the graph
+list_Outliers <- filter(list_SplitedRoad, outlier == 1)
+
+p <- p + geom_point(data = list_Outliers, aes(lon, lat), colour = "red", shape = 2)
+
+ggplotly(p)
