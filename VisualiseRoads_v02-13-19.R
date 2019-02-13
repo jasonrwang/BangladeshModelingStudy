64463de6a671f6f13d8feef8b5e548d5fa929800
Hsin Cheng <44002425+imchengh@users.noreply.github.com>
2019-02-13 12:17:09 +0100

    Create VisualiseRoads_v02-13-19.R

---
+++ VisualiseRoads_v02-13-19.R
@@ -0,0 +1,34 @@
+# Data Cleaning for Road Data
+
+# Set working directory
+setwd("D:/EPA/EPA Semester-1.3/EPA1351 Advanced Discrete Simulation/Assignment 1/_Practice")
+
+# Load library
+library(dplyr)
+library(ggplot2)
+library(plotly)
+library(googleVis)
+
+# Load data
+df <- read.csv("Roads_InfoAboutEachLRP.csv")
+
+# Create a full list of roads
+ListOfRoad <- unique(df$road)
+ListOfRoad <- data.frame(road = ListOfRoad, RoadType = substring(ListOfRoad, 1, 1))
+
+# Split raw dataframe into lists by road
+df_split <- split(df, df$road)
+
+# Modify here the road name! 
+list_SplitedRoad <- select(df_split$N102, lon, lat)
+
+# Plot the road with paths linked between points
+p <- ggplot(list_SplitedRoad, aes(lon, lat)) +
+  geom_point(shape = 1) + 
+  geom_path() + 
+  ggtitle("list_SplitedRoad")
+
+p <- ggplotly(p)
+
+p
+       
