bba8874a33c74b259d3319b7e63e635501f4e4f6
jasonrwang <accounts@jrwang.ca>
2019-02-19 16:17:17 +0100

    Modularized scripts into functions

--- scripts/CleanRoadData.R
+++ scripts/CleanRoadData.R
@@ -1,22 +1,113 @@
-### Overall Function to Clean Roads
+######################################## 
+#
+#   Overall Function to Clean Road Data
+#
+#   This code:
+#     1. Loads the raw data
+#     2. Group by road
+#     3. Delta Distance Method
+#       a. Checks for the change in distance between all points by coordinates and by chainage
+#       b. Checks the difference between these two methods and identifies outliers
+#       c. Fix outliers using interpolation method
+#     4. General Cleaning
+#       a. Assumes differences larger than .5º is a  ±1º typo, and corrects this to match other data
+#       b. If differences are less than .5º, then interpolate
+#     5. Check start and end points
+#       a. If outlier, remove
+#       b. Grouped
+#     6. Cook's Method
+#     Remove large outliers
+#
+#   To-do
+#     a. Automatically fix sequential starts (needs to be at the beginning)
+########################################
 
-# Load libraries and other scripts
+# ===================================
+#   Load libraries and other scripts
+# ===================================
+
 library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(plotly)
+library(geosphere)
+# library(xlsx)
 
 source('scripts/RoadDataFunctions.R')
 
-# Load data
-df <- read.csv('data/Roads_InfoAboutEachLRP.csv', stringsAsFactors = FALSE ) %>% group_by(road)
+## Load data
+df <- read.csv('data/Roads_InfoAboutEachLRP.csv',
+  stringsAsFactors = FALSE ) %>% group_by(road)
 
-VisualizeRoads(df, c('N2'))
+# Visually inspect roads
+VisualizeRoads(df, c('N2'), 'Initial Inspection')
 
-# Run different outlier detection techniques
+# ==================================
+#   Run different outlier detection
+#   and cleaning techniques
+# ==================================
+
+#  1. Fix chainage reversal/duplicates problem 
+df1 <- ByChainage(df)
+VisualizeRoads(df1, c('N602'),'Before Cleaning')
+
+# Detection methods
 q <- 0.99 # The quantile value we want to use
-df <- RoadCooksDis_LatLon(df)  
+# Cleans by Cook's Distance without splitting by road
+df <- df %>% RoadCooksDis_LatLon() %>% CooksDistanceClean()
 df <- RoadDeltaLon(df, q)
 df <- RoadDeltaLat(df, q)
 
-# Call out the outliers
-# df <- mutate(df, outlier = cookd > 0.05 | deltaLat > quantLimitLat | deltaLon > quantLimitLon)
+VisualizeRoads(df, c('N2'), 'Outliers', outlier = TRUE)
 
-VisualizeRoads(df, c('N2'), outlier = TRUE)
+# Create a new empty log for cleaning
+cat("CLEANING RECORD STARTS\n\n\n", file = "CleaningRecord.txt", append = FALSE)
+
+
+# =========================
+#  Data cleaning for Roads 
+# =========================
+
+# Split the data frame by road
+df2 <- split(df1, df1$road)
+
+# Clean the dataset
+df3 <- lapply(df2, fun_road)
+
+# Clean the dataset
+df4 <- lapply(df3, fun_endpt)
+
+# Clean the dataset
+df4 <- lapply(df4, fun_cooksD)
+
+# Plot a road to check the results
+VisualizeRoads(df3, c('N102'),'After Cleaning')
+
+# Reshape the dataset
+df5 <- bind_rows(df4)
+
+# =====================
+#  Write data to file 
+# =====================
+
+# Write Roads_InfoAboutEachLRP file
+write.csv(df5, file = "Roads_InfoAboutEachLRP.csv", col.names = TRUE, row.names = FALSE)
+
+df6 <- as.character(unique(df5$road))
+df6 <- as.data.frame.vector(df6)
+colnames(df6) <- "road"
+
+title <- c("road", "lrp1", "lat1", "lon1", "lrp2", "lat2", "lon2")
+write.table(t(title), file = "_roads.tcv", sep = "\t", col.names = FALSE, row.names = FALSE)
+
+for (ii in 1:nrow(df6)) {
+  vec_road <- c(df6[ii, 1])
+  df_road <- filter(df5, road == vec_road)
+  for (n in 1:nrow(as.data.frame(df3[ii]))) {
+    #print(as.character(df_road$lrp[n]))
+    if(!is.na(df_road$lat[n])){
+      vec_road <- cbind(vec_road, as.character(df_road$lrp[n]), df_road$lat[n], df_road$lon[n])
+    }
+  }
+  write.table(as.array(vec_road), file = "_roads.tcv", sep = "\t", append = TRUE, col.names = FALSE, row.names = FALSE)
+}
--- scripts/RoadDataFunctions.R
+++ scripts/RoadDataFunctions.R
@@ -1,15 +1,14 @@
 ### Functions to support CleanRoadData.R
 
-## Different Outlier Detection Methods
-
-# Shortest Distance?
+# ========================================
+#   Different Outlier Detection Methods
+# ========================================
 
 # Cook's Distance
 
 RoadCooksDis_LatLon <- function(df) {
     df <- df %>% mutate(
-        cookd = cooks.distance(lm(lat ~ lon)), # Also find each datapoint's Cook's Distance within each group
-        outlier = cookd > 0.05
+        cookd = cooks.distance(lm(lat ~ lon)) > 0.05
     )
     return(df)
 }
@@ -35,7 +34,7 @@ RoadDeltaLat <- function(df, q = 0.95) {
         deltaLat = ifelse(row_number() == 0,0,abs(lead(lat) - lat)), # Find abs change in lon
         deltaLat = ifelse(is.na(deltaLat), 0, deltaLat),
         quantLimitLat = quantile(deltaLat, q),
-        #outlier = deltaLat > quantLimitLat
+        # outlier = deltaLat > quantLimitLat
     )
     quantLimitLat_All <- quantile(df$deltaLat,q)
     df <- df %>% mutate(
@@ -44,19 +43,184 @@ RoadDeltaLat <- function(df, q = 0.95) {
     return(df)
 }
 
-## Visualizing Road Data to Look for Outliers
+ByChainage <- function(df) {
+    df <- df %>% mutate(chainage2 = lead(chainage))
 
-# Load necessary libraries
-library(ggplot2)
-library(plotly)
+    id_chainage <- c(0)
 
-VisualizeRoads <- function( df, roads = c('N6'), outlier = FALSE ) {
+    for (nrr in 1:nrow(df)) {
+        if(is.na(df$chainage2[nrr] == TRUE)){
+            # Here it is the last data point of the road
+        } else if(df$chainage[nrr] < df$chainage2[nrr]){
+            # Normal data point: The next chainage value is larger than this one
+        } else {
+            # Record the index of the point
+            id_chainage <- c(id_chainage, nrr)
+        }
+    }
+
+    # Remove the "bad" chainage datapoints
+    df <- df[-id_chainage, ]
+
+    # Remove the "chainage2" column
+    df <- df[1:7]
+    return(df)
+}
+
+# ==============================
+#   Different Cleaning Methods
+# ==============================
+
+# Modify by +1/-1 (data misentry) OR interpolate (noise)
+fun_pt <- function(p, q, r){
+    ## p,q,r are vectors of all data points we want to fix
+    # p is a specific data point
+    # q precedes p and r follows p
+    # [road  chainage  lrp lat lon type  name]
+
+    ## First check for outliers based on data misentry (e.g. 23 instead of 22)
+    # Column 4 and 5 are lon/lat
+    if(abs(as.double(p[4]) - as.double(q[4])) > 0.5){
+        p[4] <- p[4] - as.integer(p[4]) + as.integer(q[4])
+        cat(c("Fixed to:\n p1: ", as.double(p[5]), "\t", as.double(p[4]), "\n\n"), file = "CleaningRecord.txt", append = TRUE)
+    } else if (abs(as.double(p[5]) - as.double(q[5])) > 0.5) {
+        p[5] <- p[5] - as.integer(p[5]) + as.integer(q[5])
+        cat(c("Fixed to:\n p1: ", as.double(p[5]), "\t", as.double(p[4]), "\n\n"), file = "CleaningRecord.txt", append = TRUE)
+    } else {
+        ## Then deal with the rest of the outliers as noise
+
+        # Check lat moving trimmean
+        if(abs(mean(c(p$lat, q$lat, r$lat)) - p$lat) > 0.01){
+        # if the mean value with and without the point differs much
+        # the point is an outlier (peak)
+        # replace the value with the trimmean value
+        p$lat <- mean(c(q$lat, r$lat))
+        cat(c("Fixed to (TRIMMEAN):\n p1: \n", p$lat, "\t", p$lon, "\n\n"), file = "CleaningRecord.txt", append = TRUE)
+        }
+        
+        # Check lon moving trimmean
+        if(abs(mean(c(p$lon, q$lon, r$lon)) - p$lon) > 0.045){
+        # if the mean value with and without the point differs much
+        # the point is an outlier (peak)
+        # replace the value with the trimmean value
+        p$lon <- mean(c(q$lon, r$lon))
+        cat(c("Fixed to (TRIMMEAN):\n p1: \n", p$lat, "\t", p$lon, "\n\n"), file = "CleaningRecord.txt", append = TRUE)
+        }
+    }
+    return(p)
+}
+
+# Create a function to run through each road
+fun_road <- function(df){
+    # df is the dataset of (each) road
+    #print(df)  # @Unhash for debugging@
+
+    # If the road contains only one data point, then break for-loop
+    if( nrow(df) <= 2 ){
+    } else {
+        for (nn in 2:(nrow(df)-1)) {
+            # for each row / data point in the specific road
+
+            # compute the distance d1 & d2 w.r.t the next data point
+            #     d1 : distance based on chainage
+            #     d2 : distance based on coordinates
+            
+            d1 <- df$chainage[nn] - df$chainage[nn-1]
+            d2 <- distm(c(df$lon[nn-1], df$lat[nn-1]),
+                        c(df$lon[nn], df$lat[nn]),
+                        fun = distHaversine) / 1000
+            #cat(c(d1, d2, as.character(df$road[nn]), nn, df$chainage[nn]))  # @Unhash for debugging@
+            
+            # Compare the two distances
+            if(abs(d1 - d2) > 3){
+            # The difference is beyond limit, i.e., the data point is an outlier
+            cat(c(as.character(df$road[nn]), "\tn = ", nn, "\t p1 chainage @ ", df$chainage[nn], "\n",
+                    "p1: ", df$lon[nn], "\t", df$lat[nn], "\n", 
+                    "p2: ", df$lon[nn-1], "\t", df$lat[nn-1], "\n",
+                    "d1 = ", d1, ", d2 = ", d2, "\n"), file = "CleaningRecord.txt", append = TRUE)
+            
+            # Fix the data point
+            df[nn, ] <- fun_pt(df[nn, ], df[nn-1, ], df[nn+1, ])
+            }
+        }
+    }
+    # return the cleaned road dataset
+    return(df)
+}
+
+# Drop start/end outlier
+fun_endpt <- function(df) {
+  # df is the dataset of (each) road
+  
+  # If the road contains only one data point, then skip
+  if( nrow(df) <= 2 ){
+    # Skip the road dataset
+  } else {
+    # Start point outliers
+    # compute the distance d1 & d2 w.r.t the next data point
+    #     d1 : distance based on chainage
+    #     d2 : distance based on coordinates
+    
+    d1 <- df$chainage[2] - df$chainage[1]
+    d2 <- distm(c(df$lon[1], df$lat[1]),
+                c(df$lon[2], df$lat[2]),
+                fun = distHaversine) / 1000
+    
+    # Compare the two distances
+    if(abs(d1 - d2) > 3){
+      # The difference is beyond limit, i.e., the data point is an outlier
+      cat(c(as.character(df$road[2]), "\t Start point removed: \n",
+            "p: ", df$lon[2], "\t", df$lat[2], "\n",
+            "d1 = ", d1, ", d2 = ", d2, "\n\n"), file = "CleaningRecord.txt", append = TRUE)
+      
+      # Remove the point
+      df <- df[-c(1), ]
+    }
+    
+    # End point outliers
+    # compute the distance d1 & d2 w.r.t the next data point
+    #     d1 : distance based on chainage
+    #     d2 : distance based on coordinates
+    
+    d1 <- df$chainage[nrow(df)] - df$chainage[nrow(df)-1]
+    d2 <- distm(c(df$lon[nrow(df)-1], df$lat[nrow(df)-1]),
+                c(df$lon[nrow(df)], df$lat[nrow(df)]),
+                fun = distHaversine) / 1000
+    
+    # Compare the two distances
+    if(abs(d1 - d2) > 3){
+      # The difference is beyond limit, i.e., the data point is an outlier
+      cat(c(as.character(df$road[nrow(df)]), "\t End point removed: \n",
+            "p: ", df$lon[nrow(df)], "\t", df$lat[nrow(df)], "\n",
+            "d1 = ", d1, ", d2 = ", d2, "\n\n"), file = "CleaningRecord.txt", append = TRUE)
+      
+      # Remove the point
+      df <- df[-c(nrow(df)), ]
+    }
+  }
+  return(df)
+}
+
+CooksDistanceClean <- function(df) {
+    CooksRows <- which(df$cookd) # Find outlier row numbers
+    for (i in CooksRows){
+        cat(c(as.character(df$road[i]), "\t Remove CooksD:\n p: \t", df$lon[i], "\t", df$lat[i], "\n\n"), file = "CleaningRecord.txt", append = TRUE)
+    }
+    df <- df[-CooksRows,]
+}
+
+# =================
+#   Visualization
+# =================
+
+VisualizeRoads <- function( df, roads = c('N6'),
+        title = 'INSERT TITLE',outlier = FALSE ) {
     # Plot the road with paths linked between points
     roads  # Plot selected roads
     p <- ggplot(filter(df, road %in% roads), aes(lon, lat)) +
     geom_point(shape = 1) + 
     geom_path() + 
-    ggtitle("Road")
+    ggtitle(title)
 
     if (outlier) {
        list_Outliers <- filter(df, outlier == TRUE, road %in% roads)
