b277a0ff8acbe317bd339c05b24f644844694949
jasonrwang <accounts@jrwang.ca>
2019-03-18 18:07:55 +0100

    Added more traffic data

--- scripts/TrafficDataCleaning.R
+++ scripts/TrafficDataCleaning.R
@@ -3,12 +3,12 @@ library(tidyr)
 source("scripts/TrafficDataFunctions.R")
 
 # Get the names of all the traffic webpage files in the "data" subfolder
-filenames <- list.files("data", pattern = "*.traffic.htm", full.names = TRUE)
+filenames <- list.files("data/traffic", pattern = "*.traffic.htm", full.names = TRUE)
 # Get the actual names of all the roads that have been imported
-roadnames <- gsub("data/", "", gsub(".traffic.htm", "", filenames))
+roadnames <- gsub("data/traffic/", "", gsub(".traffic.htm", "", filenames))
 
 # Scrape all of the road files that are available into dfs
-ldf <- lapply(filenames, ScrapeTraffic)
+ldf <- lapply(filenames, ScrapeTraffic) # This is still a little buggy with small roads
 # Name each df in ldf by its actual name
 names(ldf) <- roadnames
 
