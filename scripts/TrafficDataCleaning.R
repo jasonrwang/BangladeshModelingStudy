f30f60bdfb7f474044037b2bd17bc2e406dd8293
jasonrwang <accounts@jrwang.ca>
2019-03-18 16:28:30 +0100

    Now scrapes all roads files in data subfolder

--- scripts/TrafficDataCleaning.R
+++ scripts/TrafficDataCleaning.R
@@ -1,4 +1,17 @@
+library(tidyr)
+
 source("scripts/TrafficDataFunctions.R")
 
-# Automate this so it works for any road!
-df1 <- ScrapeTraffic("data/N1.traffic.htm")
+# Get the names of all the traffic webpage files in the "data" subfolder
+filenames <- list.files("data", pattern = "*.traffic.htm", full.names = TRUE)
+# Get the actual names of all the roads that have been imported
+roadnames <- gsub("data/", "", gsub(".traffic.htm", "", filenames))
+
+# Scrape all of the road files that are available into dfs
+ldf <- lapply(filenames, ScrapeTraffic)
+# Name each df in ldf by its actual name
+names(ldf) <- roadnames
+
+# Summary statistics for all the roads
+res <- lapply(ldf, summary)
+names(res) <- substr(filenames, 6, 30)
