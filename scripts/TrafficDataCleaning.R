9110680b8bac599f0defce7785ea60bf61798e6a
jasonrwang <accounts@jrwang.ca>
2019-03-19 16:19:27 +0100

    Changed 6aeb413 `for` loop to `do.call`

--- scripts/TrafficDataCleaning.R
+++ scripts/TrafficDataCleaning.R
@@ -8,20 +8,11 @@ filenames <- list.files("data/traffic", pattern = "*.traffic.htm", full.names = 
 # Get the actual names of all the roads that have been imported
 roadnames <- gsub("data/traffic/", "", gsub(".traffic.htm", "", filenames))
 
-
-## Scrape all of the road files that are available into dfs
-
-# Create an empty dataset
-df <- c(0)
-
-# Scrape each road file into df
-for (file in filenames) {
-  df <- rbind(df, ScrapeTraffic(file))
-}
-
-# Remove the empty dummy row
-df <- df[-1, ]
-
+# Scrape all of the road files that are available into dfs
+df <- lapply(filenames, ScrapeTraffic) # This is still a little buggy with small roads
+# Merge this list of dfs into one large df
+df <- do.call(rbind,df)
+df <- df %>% group_by(Road)
 
 ## Write to file
 write.csv(df, file = "data/Traffic.csv")
