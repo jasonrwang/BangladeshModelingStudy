5fcd9d58f740877d87b375a943331dac094b55fa
Hsin Cheng <44002425+imchengh@users.noreply.github.com>
2019-03-20 17:26:45 +0100

    Update visualistion scripts

--- scripts/TrafficDataFunctions.R
+++ scripts/TrafficDataFunctions.R
@@ -5,7 +5,6 @@ library(tidyr)
 importWidth <- function(file) {
     df <- read.table(file,header = TRUE, sep = "\t")
     names(df) <- c("Road", "roadID", "Chainage.start", "Chainage.end", "width", "nrLanes")
-    
     return(df)
 }
 
@@ -71,66 +70,4 @@ ScrapeTraffic <- function(file) {
     )
 
     return(df)
-}
-
-## Calcualte weighted nrLanes for roadRow data
-nrLanesW <- function(rrRow) {
-  # Create an empty vector for storing target indice of nrLanes dataset
-  id <- c()
-  
-  # Get row index for input road row data
-  jj <- which(!is.na(match(df_rr$chainage, as.numeric(rrRow[2]))))
-  
-  # Calculate the chainage values of current and next node
-  rr1 <- as.numeric(df_rr[jj  , 2])
-  rr2 <- as.numeric(df_rr[jj+1, 2])
-  #print(c(rr1, rr2))   # for debugging
-  
-  # Compare the chainage values with that of nrLanes dataset
-  for (ii in 1:nrow(df_rr_nrLanes)) {
-    # If looped to the last row
-    if (is.na(as.numeric(df_rr_nrLanes$Chainage.start[ii+1]))) {
-      id <- c(id, ii)
-      break
-    }
-    # Otherwise, start finding targeted index ii
-    if (rr1 < as.numeric(df_rr_nrLanes$Chainage.start[ii+1])) {
-      # Link startChainage >= startChainage of nrLanesRow at index = ii
-      # Hence the index = ii is within scope
-      id <- c(id, ii)  # add the index to target vector
-      
-      # Check the chainage of next node
-      if (rr2 < as.numeric(df_rr_nrLanes$Chainage.end[ii])) {
-        # Link endChaninage <= endchainage of nrLanesRow at index = ii
-        # Type-2 Link
-        #print(c(rr1, rr2))          # for debugging
-        #print(df_rr_nrLanes[id, ])  # for debugging
-        break
-      } else if (rr2 < as.numeric(df_rr_nrLanes$Chainage.end[ii+1])) {
-        # Link endChaninage <= endchainage of nrLanesRow at index = ii+1
-        # Type-1a or -1b Link
-        id <- c(id, ii+1)  # add the index to target vector
-        #print(c(rr1, rr2))          # for debugging
-        #print(df_rr_nrLanes[id, ])  # for debugging
-        break
-      }
-    }
-    ii =+ 1
-  }
-  # Found the target nrLanes sub-dataset for input road link
-  rr_nrLanes <- df_rr_nrLanes[id, ]
-  
-  # Replace the very start and end chainage values
-  rr_nrLanes[1, 3] <- rr1
-  rr_nrLanes[nrow(rr_nrLanes), 4] <- rr2
-  #print(c(rr1, rr2)) # for debugging
-  #print(rr_nrLanes)  # for debugging
-  
-  # Calculate weighted nrLanes as sumproduct
-  rr_nrLanes <- rr_nrLanes %>% 
-    transmute(nrLanesW = (Chainage.end - Chainage.start) * nrLanes) %>%
-    as.vector() %>% sum() %>% as.numeric() / (rr2 - rr1)
-  
-  # Return weighted nrLanes
-  return(rr_nrLanes)
 }
--- scripts/TrafficFlowsVisualising.R
+++ scripts/TrafficFlowsVisualising.R
@@ -8,12 +8,12 @@ library(plotly)
 library(leaflet)
 library(shiny)
 
-source("scripts/TrafficDataFunctions.R")
+source("scripts/TrafficFlowsVisualisingFunction.R")
 
 # Load data
 df <- read.csv('data/_roads3.csv')
 df_nrLanes <- read.csv('data/Width.csv') %>% select(-X)
-df_traffic <- read.csv('data/Traffic.csv') %>% select(-X)
+df_traffic <- read.csv('data/Traffic.csv', stringsAsFactors = FALSE) %>% select(-X)
 df_bridge <- readxl::read_excel('data/BMMS_overview.xlsx')
 
 # Assign candidate road
@@ -22,9 +22,18 @@ cRoad = c("N1")
 # Assign transport mode
 tMode <- c('Heavy.Truck')
 
+
 ## =============================
 ##  Data prep for visualisation 
 ## =============================
+
+
+
+
+
+
+
+
 
 ## Subset the data for candidate road
 df_rr <- df %>% filter(road == cRoad)
@@ -43,85 +52,101 @@ df_rr$nrLanesW <- df_rr %>% apply(1, nrLanesW)
 ##  Merge traffic flow dataset 
 ## ============================
 
-## Modify dtype for traffic flow dataset (from factor to numeric)
+# Make a list of traffic flow vol colnames
+colnameTraffic <- c("Heavy.Truck", "Medium.Truck", "Small.Truck",
+                    "Large.Bus", "Medium.Bus","Micro.Bus", 
+                    "Utility", "Car", "Auto.Rickshaw", 
+                    "Motor.Cycle", "Bi.Cycle", "Cycle.Rickshaw", "Cart",
+                    "Motorized", "Non.Motorized", "Total.AADT", "PCE") %>% as.array()
 
-# Get the columns whose dtype is factor
-indx <- sapply(df_rr_traffic, is.factor)
-
-# Remove the ones that should have dtype as factor
-indx[c('Road', 'Description', 'LRP.start', 'LRP.end', 'Road.Code', 'Side')] <- FALSE
-
-# Fix the dtype to numeric
-df_rr_traffic[indx] <- lapply(df_rr_traffic[indx], function(x) as.numeric(as.character(x)))
-
-## Sum the AADT of L/R components for link
-df_rr_trafficSum <- df_rr_traffic %>% select(-c(1:9, 26, 28)) %>% 
+# Prepare the RHS-dataset (sum of traffic flow vol of L/R components for link)
+df_rr_trafficSum <- df_rr_traffic %>% select(c(colnameTraffic, 'Segment')) %>%
+  sapply(function(x) as.numeric(x)) %>% as.data.frame() %>% 
   group_by(Segment) %>% summarise_all(sum)
 
-df_rr_trafficSum <- df_rr_traffic %>% select(c(1:9, 26:27)) %>% 
+# Merge the LHS-dataset (first chainage dtRow for each link) with prepared RHS-dataset 
+df_rr_trafficSum <- df_rr_traffic %>% select(LRP.start, Chainage.start, Road.Code, Segment) %>% 
   group_by(Segment) %>% top_n(1, Road.Code) %>%
-  right_join(df_rr_trafficSum, by = 'Segment') %>%
-  select(-Road)
+  right_join(df_rr_trafficSum, by = 'Segment')
 
 ## Merge with road dataset
-
 df_rr <- df_rr %>% full_join(df_rr_trafficSum, 
-                              by = c("chainage" = "Chainage.start"))
+                             by = c("chainage" = "Chainage.start"))
 
 # Fill in NAs with latest non-NA row (startChainage of road link)
-df_rr <- df_rr %>% fill(colnames(df_rr)[9:length(df_rr)]) %>% drop_na()
+df_rr <- df_rr %>% fill(colnames(df_rr)[which(colnames(df_rr) == "LRP.start") : length(df_rr)])
+
+# Drop the roadRow where nrLanes = 0
+df_rr <- df_rr[-which(df_rr$nrLanesW == 0), ]
+
 
 ## ===========================
 ##  Calculate traffic density  
 ## ===========================
 
 ## Traffic density = AADT / nrLanes (weighted)
-df_rr[19:35] <- df_rr[19:35] / df_rr$nrLanesW
+df_rr[colnameTraffic] <- df_rr[colnameTraffic] / df_rr$nrLanesW
+
+## =========================================
+##  Calculate vulnerability (per road link) 
+## =========================================
+
+#BridgeVul(df_rr, df_rr_bridge)
+#unique(a$Vulnerability)
+
 
 ## ===============================================================
 ##  Visualising N1 (or candidate road) Traffic per transport mode 
 ## ===============================================================
 
-# Subset the dataset for assigned transport mode
-df_rrV <- df_rr %>% select(c('chainage', 'lat', 'lon', tMode)) %>% 
-  data.table::setnames(old = tMode, new = 'TrafficDensity')
 
-# Modify the dtype of bridge condition from character to factor
-df_rr_bridge$condition <- factor(df_rr_bridge$condition)
-
-## Plot the roads and bridges on map of Bangladesh
-get_stamenmap(bbox = c(left  = 87.8075, bottom = 20.5845, 
-                       right = 92.8135,    top = 26.7182), 
-              zoom = 7, maptype = "terrain", color = 'bw') %>% ggmap() +
-  geom_point(data = df_rrV, aes(x = lon, y = lat, colour = TrafficDensity)) +
-  scale_colour_gradient(name = tMode, guide = 'colorbar', low = 'pink', high = 'red') +
-  #geom_point(data = df_rr_bridge, aes(x = lon, y = lat, shape = condition), size = 1.5) +
-  #scale_shape_manual(name = 'Bridge category', guide = 'legend', values = c(0, 1, 2, 5)) +
-  ggtitle(paste('Traffic density (AADT / nrLanes) for ', cRoad))
-
-
-# Plot base map of bangladesh
 
 # Create a palette that maps bridge condition factor levels to colors
-pal <- colorFactor(c("grey", "yellow", "orange", "red"), 
-                   domain = c("A", "B", "C", "D"))
+palBridge <- colorFactor(c("grey", "yellow", "orange", "red"), 
+                         domain = c("A", "B", "C", "D"))
+# Create a palette that maps road traffic to continuour colors
+palRoad <- colorNumeric("GnBu", df_rr[tMode], na.color = NA)
 
-m <- leaflet( df_rr_bridge ) %>% addTiles() %>% 
-  # Overlay groups - 
-  addCircleMarkers(~lon, ~lat, 
+m <- leaflet() %>% addTiles(group = "General map") %>% 
+  addProviderTiles(providers$CartoDB.Positron, group = "CartoDB (Default)") %>%
+  # Overlay groups - road data
+  addCircleMarkers(data = df_rr, ~lon, ~lat,
+                   label = ~as.character(lrp),
+                   radius = 5,
+                   weight = 1,
+                   color = 'grey80',
+                   fillColor = ~palRoad(Heavy.Truck), # NEED fix - to tMode
+                   fillOpacity = 1,
+                   group = "Road"
+                   ) %>%
+  addLegend("bottomright", 
+            pal = palRoad, 
+            values = df_rr$Heavy.Truck,
+            group = "Road",
+            title = "Road") %>%
+  
+  # Overlay groups - by bridge condition A/B/C/D
+  addCircleMarkers(data = df_rr_bridge, ~lon, ~lat, 
                    label = ~as.character(condition), 
                    popup = ~as.character(LRPName),
-                   radius = 5,
-                   color = 'grey50',
+                   radius = 4,
                    weight = 1,
-                   fillColor = ~pal(condition), 
+                   color = 'black',
+                   fillColor = ~palBridge(condition), 
                    fillOpacity = ~ifelse(condition == "A", 0.5, 0.9),
                    group = ~as.character(condition)
                    ) %>% 
+  addLegend("bottomright", 
+            pal = palBridge, 
+            values = df_rr_bridge$condition,
+            group = c("A", "B", "C", "D"),
+            title = "Bridge condition") %>%
   addLayersControl(
-    overlayGroups = c("A", "B", "C", "D"),
-    options = layersControlOptions(collapsed = FALSE)
+    baseGroups = c("CartoDB (Default)", "General map"),
+    overlayGroups = c("A", "B", "C", "D", "Road"),
+    options = layersControlOptions(collapsed = FALSE),
+    position = "topleft"
   ) %>%
   setView(lng = 90.3, lat = 24, zoom = 7)
 
-m %>% addProviderTiles(providers$CartoDB.Positron)
+m
