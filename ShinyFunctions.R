a1dc21bb5932389eff52c3ee8cc19182a8bef640
jasonrwang <accounts@jrwang.ca>
2019-04-06 10:47:18 +0200

    Working vis of refreshing data from SQL

--- ShinyFunctions.R
+++ ShinyFunctions.R
@@ -1,42 +1,32 @@
 # Create temporary file to write outputs to
-outputfile = "LRP,lat,lon,condition,segment,row_names,ID,Time,TrafficTruck,TrafficBus,TrafficCar,TrafficMotorbike,TrafficBicycle,PCE\n"
-cat(outputfile, file = 'data/RealTimeVis.csv', sep = ",")
-
-GetLastHour <- function(filename) {
-    dfDisplay <- read.csv(filename, header = TRUE, stringsAsFactors = FALSE)
-    latest_hour <- max(dfDisplay$Time)
-    dfDisplay <- dfDisplay  %>% filter(Time == latest_hour)
-    return(dfDisplay)
+clearOutputFile <- function() {
+    outputfile = "LRP,lat,lon,condition,segment,row_names,ID,Time,TrafficTruck,TrafficBus,TrafficCar,TrafficMotorbike,TrafficBicycle,PCE\n"
+    cat(outputfile, file = 'data/RealTimeVis.csv', sep = ",")
 }
 
-a <- GetLastHour('data/RealTimeVis.csv')
-
-updateSQLtraffic <- function(read_ID) {
-    # Read Simio output (from MySQL)
-    # Append only the latest data though!
-
-    # Don't read the same hour multiple times
+newSQLdata <- function() {
+    # Wait until the file is ready
+    if (dbGetQuery(conn, "SElECT COUNT(*) FROM lab4test") == 0) {
+        print("newSQLdata: Empty DB")
+        return(0)
+    }
+    # Used in a reactivePoll –> needs to return a new value to signal it to change!
     latest_ID <- dbGetQuery(conn, "SElECT ID FROM lab4test ORDER BY ID DESC LIMIT 1") # Find the latest hour written/this is a fast function
-    print(c("Comparison read_ID", read_ID))
-    if (latest_ID - read_ID < 2) {
-        return(read_ID)
-        # Act only once the last hour is completed writing to MySQL!
-        # SQL_length <- dbGetQuery(conn, paste("SELECT COUNT(*) FROM", db_table )) # Query MySQL for the dimensions of the table
-    } else {
-        print("TIME TO WRITE")
-        print(c("latest_ID", latest_ID))
-        print(c("read_ID", read_ID))
-        read_ID <- latest_ID - 1
-        print(c("new read_ID", read_ID))
-    
-        query <- paste("SELECT * FROM", db_table,
+    return(latest_ID)
+}
+newSQLdata()
+
+GetLatestHour <- function(filename) {
+    # When the latest_ID changes read in the latest data and write it to a CSV
+    latest_ID <- dbGetQuery(conn, "SElECT ID FROM lab4test ORDER BY ID DESC LIMIT 1") # Find the latest hour written/this is a fast function
+    read_ID <- latest_ID - 1
+    print(c("GetLatestHour read_ID:", read_ID))
+
+    # Fetch the completed data from SQL
+    query <- paste("SELECT * FROM", db_table,
             "WHERE ID =", read_ID)
-        SimioOutput <- dbGetQuery(conn, query)
-        # Flush results
-        if (length(dbListResults(conn)) > 0) {
-            dbClearResult(dbListResults(conn)[[1]])
-        }
-        SimioOutput <- SimioOutput %>% group_by(Time) %>%
+    SimioOutput <- dbGetQuery(conn, query)
+    SimioOutput <- SimioOutput %>% group_by(Time) %>%
             mutate(
                 PCE = TrafficTruck * PCE_Truck +
                 TrafficBus * PCE_Bus +
@@ -60,8 +50,6 @@ updateSQLtraffic <- function(read_ID) {
         # Save outputs into a csv to trigger reactive file reader and to save for replay later
         write.table(dfDisplay, file = 'data/RealTimeVis.csv',  # append doesn't work with write.csv
             sep = ",", append = TRUE, row.names = FALSE, col.names = FALSE)
-        print(c("returned read_ID", read_ID))
-        return(read_ID)
-    }
+        
+        return(dfDisplay)
 }
-
--- main.R
+++ main.R
@@ -1,4 +1,5 @@
-## Pseudo-code
+## Introduction
+# The one main file to rule them all
 
 # Load top-level necessary libraries
 library(dplyr)
@@ -41,11 +42,6 @@ conn <- dbConnect(RMySQL::MySQL(),
                  user=db_user,
                  password=db_password)
 
-# # Use sample results
-# SimioOutput <- read.csv('data/SimioOutput.csv', stringsAsFactors = FALSE)
-# name <- 'lab4test'
-# dbWriteTable(conn, name, SimioOutput,overwrite = TRUE)
-
 PCE_Car <- 1
 PCE_Bus <- mean(c(3, 3.29, 3.26, 3.17, 3.06))
 PCE_Truck <- mean(c(3, 3.27, 3.21, 3.33, 3.41))
@@ -64,6 +60,8 @@ brokenBridges <- c("A", "B")
 # Pass the broken bridges to MySQL
 
 # Predeclare Shiny stuff
+clearOutputFile()
+
 ui <- fluidPage(
     mainPanel(
         leafletOutput(outputId = "N1map")
@@ -79,32 +77,11 @@ ui <- fluidPage(
     )
 )
 
-read_ID <- 0
 server <- function(input, output, session) {
     # Create a reactivePoll that updates when the data changes
-    # dfDisplay <- reactiveFileReader(1000, session, 'data/RealTimeVis.csv', GetLastHour)
-    dfDisplay <- GetLastHour('data/RealTimeVis.csv')
-
-    ## Wait until the file is ready:
-    # Wait if empty DB
-    while( dbGetQuery(conn, paste("SELECT COUNT(*) FROM", db_table )) == 0 ){
-        print("Empty DB")
-        next
-    }
-     # Wait if still first hour
-    while (updateSQLtraffic(read_ID) == 0) {
-        print("First Hour")
-        next
-    }
-    
-    #function to read the DB and write to file if it's ready!
-
-    update_data <- function(read_ID) {
-      print("server read_ID")
-      print(read_ID)
-      read_ID <- updateSQLtraffic(read_ID)
-    }
-    # update_data(read_ID)
+    # newSQLdata returns the latest hour of data
+    # GetLatestHour extracts the latest *complete* hour of data once newSQL updates
+    dfDisplay <- reactivePoll(1000, session, newSQLdata, GetLatestHour)
 
     # Create a palette that maps bridge traffic levels to colors
     palNorm <- colorNumeric(
@@ -113,71 +90,21 @@ server <- function(input, output, session) {
     # Display the data
     output$N1map <- renderLeaflet({
         leaflet() %>%
-            setView(lng = 90.4, lat = 24, zoom = 7) %>%
+            setView(lng = 89.5, lat = 23.1, zoom = 6) %>%
             addProviderTiles(
                 providers$CartoDB.Positron,
                 options = providerTileOptions(noWrap = TRUE)
             ) %>%
         addCircleMarkers(
-            data = dfDisplay, ~lon, ~lat, # Add roads
+            data = dfDisplay(), ~lon, ~lat, # Add roads
             label = ~as.character(LRP),
             radius = 5,
             weight = 1,
             color = 'grey80',
-            fillColor = ~palNorm(PCE), # Make dynamic
+            fillColor = ~palNorm(PCE), # Make dynamic input$trafficType
             fillOpacity = 1,
             group = "Road"
         )
-        invalidateLater(1000,session)
-        print(c("invalidated and update_data():"))
-        update_data(read_ID)
     })
 }
 shinyApp(ui, server)
-
-
-# observe({
-        #     proxy <- leafletProxy("mymap", data = data)
-        #     proxy %>% clearMarkers()
-        #     if (input$markers) {
-        #         proxy %>% addCircleMarkers(stroke = FALSE, color = ~pal2(depth_type), fillOpacity = 0.2,
-        #                                 label = ~as.character(paste0("Magnitude: ", sep = " ", mag))) %>%
-        #         addLegend("bottomright", pal = pal2, values = data$depth_type,
-        #                     title = "Depth Type",
-        #                     opacity = 1)}
-        #     else {
-        #         proxy %>% clearMarkers() %>% clearControls()
-        #     }
-        # })
-
-    # Overlay groups - road data
-        # addLegend("bottomright", 
-        #             pal = palRoad, 
-        #             values = df_rr[[tMode]],
-        #             group = "Road",
-        #             title = "Traffic density") #%>%
-
-        # Overlay groups - by bridge condition A/B/C/D
-        # addCircleMarkers(data = df_rr_bridge, ~lon, ~lat, 
-        #                 label = ~as.character(condition), 
-        #                 popup = ~as.character(LRPName),
-        #                 radius = 4,
-        #                 weight = 1,
-        #                 color = 'black',
-        #                 fillColor = ~palBridge(condition), 
-        #                 fillOpacity = ~ifelse(condition == "A", 0.5, 0.9),
-        #                 group = ~as.character(condition)
-        #                 ) %>% 
-        # addLegend("bottomright", 
-        #             pal = palBridge, 
-        #             values = df_rr_bridge$condition,
-        #             group = c("A", "B", "C", "D"),
-        #             title = "Bridge condition") %>%
-        # addLayersControl(
-        #     baseGroups = c("CartoDB (Default)", "Openstreet map"),
-        #     overlayGroups = c("A", "B", "C", "D", "Road"),
-        #     options = layersControlOptions(collapsed = FALSE),
-        #     position = "topleft"
-        # )
-
-## Bonus 2: Allow a replay where user can change the speed
